c4db0674e81870ef3eb4efbbb5c0b96254377250
JiexianFENG <sherlockjiexian@gmail.com>
2021-08-26 00:02:35 +1000

    100%
    finished

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -7,15 +7,32 @@ public class PrintAndHide : MonoBehaviour
     public Renderer rend;
     [SerializeField]
     private int i;
-    
+    private int RandomNum;
     void Start()
     {
         i = 3 ;
+        if (gameObject.tag == "Blue")
+        {
+            RandomNum = Random.Range(200, 251);
+        }
+        
     }
 
     void Update()
-    {
-        Debug.Log(gameObject.name + ": " + (++i) );
+    {   
+        
+        Debug.Log(gameObject.name + ":" + (++i) );
+        if (gameObject.tag == "Red" & i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag == "Blue" & i == RandomNum )
+        {
+
+            rend.enabled = false;
+        
+        }
+
         
     }
 }
